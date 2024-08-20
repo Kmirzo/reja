9c273c933b624021ce1406cdbd9dbaf2a48155e4
Kamron <kamronmirzooo@gmail.com>
2024-08-20 21:40:46 +0900

    feat:A-TASK

--- train.js
+++ train.js
@@ -1,38 +1,38 @@
-console.log("Jack Ma maslahatlari");
-const list =[
-    "Yaxshi talaba boling", // 0-20
-    "Togri boshliq tanlang va koproq xato qiling", // 20-30
-    "Uzingizni ishingizni boshlang", // 30-40
-    "Siz kuchli bolgan narsalarni qiling", // 40-50
-    "Yoshlarga investitsiya qiling", // 50-60
-    "Endi dam oling foydasi yoq" // 60
-]
+// console.log("Jack Ma maslahatlari");
+// const list =[
+//     "Yaxshi talaba boling", // 0-20
+//     "Togri boshliq tanlang va koproq xato qiling", // 20-30
+//     "Uzingizni ishingizni boshlang", // 30-40
+//     "Siz kuchli bolgan narsalarni qiling", // 40-50
+//     "Yoshlarga investitsiya qiling", // 50-60
+//     "Endi dam oling foydasi yoq" // 60
+// ]
 
 // callback
-function maslahatBering (a, callback) {
-    if (typeof a !== "number") callback("insert a number", null);
-    else if (a <=20) callback(null, list[0]);
-    else if (a > 20 && a <= 30 ) callback(null, list[1]);
-    else if (a > 30 && a <= 40 ) callback(null, list[2]);
-    else if (a > 40 && a <= 50 ) callback(null, list[3]);
-    else if (a > 50 && a <= 60 ) callback(null, list[4]);
-    else{
-        setInterval(function () {
-            callback(null, list[5]);
-        }, 1000)
+// function maslahatBering (a, callback) {
+//     if (typeof a !== "number") callback("insert a number", null);
+//     else if (a <=20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30 ) callback(null, list[1]);
+//     else if (a > 30 && a <= 40 ) callback(null, list[2]);
+//     else if (a > 40 && a <= 50 ) callback(null, list[3]);
+//     else if (a > 50 && a <= 60 ) callback(null, list[4]);
+//     else{
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000)
        
-    }
+//     }
 
-}
+// }
 
-console.log('passed here 0');
-maslahatBering(70, (err, data) => {
-    if (err) console.log("ERROR:", err);
-    else{
-        console.log(data);
-    }
-})
-console.log("passed here");
+// console.log('passed here 0');
+// maslahatBering(70, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else{
+//         console.log(data);
+//     }
+// })
+// console.log("passed here");
 
 
 // Async function
@@ -77,3 +77,22 @@ console.log("passed here");
 
 // }
 // run();
+
+
+
+// A-TASK
+const harf = 'b';
+const soz = 'berib boldi';
+
+function harfTopish(harf, soz) {
+    let soni = 0;
+    for (let i = 0; i < soz.length; i++) {
+      if (soz[i] === harf) {
+        soni++;
+      }
+    }
+    return soni;
+  } 
+  const javob = harfTopish(harf, soz);
+  console.log("javob:", javob); 
+  
